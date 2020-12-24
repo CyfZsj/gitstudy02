cb6ac64869f32c23f41f6bb17b1fa0a5f926f133
cyf <1049444647@qq.com>
2020-12-24 10:44:37 +0800

    删除变量

--- src/main/java/com/cyf/study02/Study02Application.java
+++ src/main/java/com/cyf/study02/Study02Application.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Study02Application {
-    int i = 0;
-    int a = 3;
     public static void main(String[] args) {
         SpringApplication.run(Study02Application.class, args);
     }
