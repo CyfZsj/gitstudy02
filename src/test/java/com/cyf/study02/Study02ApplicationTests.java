a8f3babdd0096cea7ed414d1a54feaac3bfce2d9
cyf <1049444647@qq.com>
2020-12-24 10:18:17 +0800

    项目工程

---
+++ src/test/java/com/cyf/study02/Study02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyf.study02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Study02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
